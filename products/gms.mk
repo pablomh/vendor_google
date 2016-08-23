555be14655e66009b8400de396325e1c8b313fc3
David <Davidteri91@gmail.com>
2016-08-23 20:15:23 +0200

    vendor: goodbye 8226
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- products/gms.mk
+++ products/gms.mk
@@ -22,13 +22,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.alarm_alert=Helium.ogg
 
 # Audio (Ringtones)
-ifneq ($(filter aosp_c6903 aosp_d6503 aosp_d6603 aosp_e6653 aosp_e6853 aosp_sgp521 aosp_sgp621 aosp_d5803 aosp_e2303 aosp_d2303 aosp_d2203 aosp_d5103 aosp_d5303 aosp_e5823 aosp_d5503 aosp_c6833 aosp_sgp771, $(TARGET_PRODUCT)),)
+ifneq ($(filter aosp_c6903 aosp_d6503 aosp_d6603 aosp_e6653 aosp_e6853 aosp_sgp521 aosp_sgp621 aosp_d5803 aosp_e2303 aosp_e5823 aosp_d5503 aosp_c6833 aosp_sgp771, $(TARGET_PRODUCT)),)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=Orion.ogg
 endif
 
 # bootanimation (240-320 DPI)
-ifneq ($(filter aosp_sgp521 aosp_sgp511_windy aosp_sgp621 aosp_sgp611_windy aosp_d5803 aosp_e2303 aosp_d2303 aosp_d2203 aosp_d5103 aosp_d5303 aosp_e5823 aosp_d5503 aosp_c6833 aosp_sgp771 aosp_sgp712_windy, $(TARGET_PRODUCT)),)
+ifneq ($(filter aosp_sgp521 aosp_sgp511_windy aosp_sgp621 aosp_sgp611_windy aosp_d5803 aosp_e2303 aosp_e5823 aosp_d5503 aosp_c6833 aosp_sgp771 aosp_sgp712_windy, $(TARGET_PRODUCT)),)
 PRODUCT_COPY_FILES +=  \
     vendor/google/prebuilt/common/bootanimation/240_320/bootanimation.zip:system/media/bootanimation.zip
 endif
