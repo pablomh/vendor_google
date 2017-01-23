039a1caa9e80d32fd5cbf00e4747bb4bbacd2b7c
David <Davidteri91@gmail.com>
2017-01-24 00:39:58 +0100

    vendor: show SELinux info
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- products/gms.mk
+++ products/gms.mk
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# SELinux
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.selinux=1
+
 # Kernel-headers FLAG
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 
